bccdacbf8ecf827d75791e42ede07626f952bc19
demetris <demetris@walla.com>
2013-12-14 05:05:53 +0200

    Add signal Strength

--- ril/telephony/java/com/android/internal/telephony/X3RIL.java
+++ ril/telephony/java/com/android/internal/telephony/X3RIL.java
@@ -10,6 +10,7 @@ import android.os.Parcel;
 import android.os.SystemService;
 
 import android.telephony.PhoneNumberUtils;
+import android.telephony.SignalStrength;
 
 public class X3RIL extends RIL implements CommandsInterface {
     private boolean sentHwBootstrap = false;
@@ -129,6 +130,7 @@ public class X3RIL extends RIL implements CommandsInterface {
         }
     }
 
+	@Override
     protected RILRequest
     processSolicited (Parcel p) {
         int serial, error;
@@ -216,3 +218,80 @@ public class X3RIL extends RIL implements CommandsInterface {
     }
 
 }
+
+	@Override
+    protected Object responseSignalStrength(Parcel p) {
+        int numInts = 12;
+        int response[];
+
+        // This is a mashup of algorithms used in
+        // SamsungQualcommUiccRIL.java
+
+        // Get raw data
+        response = new int[numInts];
+        for (int i = 0; i < numInts; i++) {
+            response[i] = p.readInt();
+        }
+
+		// response[1]=0 -- gsm
+		// response[1]=99 -- 3G
+
+		/*
+		 * 	[signalStrength=137, 			-- 		response[0]
+		 *	 bitErrorRate=99,    			-- 		response[1]	            
+		 *	 CDMA_SS.dbm=-1,				-- 		response[2]
+		 *	 CDMA_SSecio=-1,    			-- 		response[3]            			
+		 *	 EVDO_SS.dbm=-1,				-- 		response[4]
+		 *	 EVDO_SS.ecio=-1,   			-- 		response[5]             
+		 *	 EVDO_SS.signalNoiseRatio=-1,   -- 		response[6]             
+		 *	 LTE_SS.signalStrength=-1,		-- 		response[7]
+		 *	 LTE_SS.rsrp=-1,				-- 		response[8]
+		 *	 LTE_SS.rsrq=-1,                -- 		response[9]
+		 *	 LTE_SS.rssnr=-1,				-- 		response[10]
+		 *	 LTE_SS.cqi=-1]					-- 		response[11]
+		 */
+	
+		int gsmSignalStrength = response[0];
+
+		// if is TD signal, do a convert
+		if (gsmSignalStrength >= 100) {
+
+		  if (gsmSignalStrength % 2 == 0) {
+
+		  	if (gsmSignalStrength <= 165) {
+
+				if (gsmSignalStrength > 103)
+					gsmSignalStrength++;
+				else
+					gsmSignalStrength = 103;
+
+			} else {
+				gsmSignalStrength = 165;
+			}
+		  }
+
+		  gsmSignalStrength = gsmSignalStrength - 103 >> 1;
+		  response[0] = gsmSignalStrength;
+		}
+
+        // RIL_LTE_SignalStrength
+        if (response[7] == 99) {
+            // If LTE is not enabled, clear LTE results
+            // 7-11 must be -1 for GSM signal strength to be used (see
+            // frameworks/base/telephony/java/android/telephony/SignalStrength.java)
+            response[8] = -1;
+            response[9] = -1;
+            response[10] = -1;
+            response[11] = -1;
+        }
+
+		boolean isGsm = (mPhoneType == RILConstants.GSM_PHONE);
+
+        return new SignalStrength(response[0], 
+			response[1], response[2], response[3], response[4], response[5], 
+			response[6], response[7], response[8], response[9], response[10], response[11], isGsm);
+
+    }
+
+}
+
